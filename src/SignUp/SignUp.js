8efab47202acec412ceb4daaa88bb6376fd0ff93
Nyssa Keller <nyssa1@gmail.com>
2018-04-13 11:35:16 -0600

    Create signup form

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,10 +1,15 @@
-import React, { Componenet } from 'react';
+import React, { Component } from 'react';
 
 class SignUp extends Component {
   constructor() {
     super();
     this.state = {
-
+      username: '',
+      password: '',
+      verifyPassword: '',
+      name: '',
+      location: '',
+      phoneNumber: ''
     }
   }
 
@@ -13,10 +18,39 @@ class SignUp extends Component {
       <div>
         <form>
           <input type='text'
+                 placeholder='Username'
+                 value={this.state.username}
+                 id='username-input'
+          />
+          <input type='text'
+                 placeholder='Password'
+                 value={this.state.password}
+                 id='password-input'
+          />
+          <input type='text'
+                 placeholder='Password'
+                 value={this.state.verifyPassword}
+                 id='verify-password-input'
+          />
+          <input type='text'
                  placeholder='Restaurant Name'
+                 value={this.state.name}
+                 id='name-input'
+          />
+          <input type='text'
+                 placeholder='Address'
+                 value={this.state.location}
+                 id='location-input'
+          />
+          <input type='text'
+                 placeholder='Phone Number'
+                 value={this.state.phoneNumber}
+                 id='phonenumber-input'
           />
         </form>
       </div>
     )
   }
 }
+
+export default SignUp;
