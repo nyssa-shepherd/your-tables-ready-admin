e0a765a57c5a24289e334c216a3b4f1f5d8f6ea9
Nyssa Keller <nyssa1@gmail.com>
2018-04-14 22:46:01 -0600

    Set up tables as a class

---
+++ src/Tables/Tables.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Tables extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Tables;
