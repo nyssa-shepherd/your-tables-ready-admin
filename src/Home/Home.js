d5e86fa6a9f76a4442c0bf626ddf9c27393f1097
Nyssa Keller <nyssa1@gmail.com>
2018-04-14 20:50:42 -0600

    Style sign up form

---
+++ src/Home/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Reserant</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
