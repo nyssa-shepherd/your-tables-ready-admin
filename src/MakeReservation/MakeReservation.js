ac83314e807eb608a2d5146e07814dc32b42c138
Nyssa Keller <nyssa1@gmail.com>
2018-04-14 22:48:19 -0600

    Create MakeReservation component

---
+++ src/MakeReservation/MakeReservation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MakeReservation extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default MakeReservation;
