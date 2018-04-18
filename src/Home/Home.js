ae266da5e73bda26284a11539fa095e6568440a8
Nyssa Keller <nyssa1@gmail.com>
2018-04-18 17:59:41 -0600

    Conditionally render sign up screen based on if there is a user in state

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import SignUp from '../SignUp/SignUp';
 import './Home.css';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
-      user: ''
+      user: null
     }
   }
   
   render() {
+    const homeView = !this.state.user ? <SignUp /> : <h1>Your Table's Ready</h1>
     return (
       <div>
-        <h1>Your Table's Ready</h1>
-        <div className='screen'></div>
+        <div className='screen'>{homeView}</div>
       </div>
     );
   }
