0854d472e12c0c08ad5df62756ff1e6cc4b6098b
Nyssa Keller <nyssa1@gmail.com>
2018-04-18 19:16:26 -0600

    Start to create fn in SignIn that fetches restaurants from the database. Work in Progress

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -4,26 +4,49 @@ class SignIn extends Component {
   constructor() {
     super();
     this.state = {
-
+      username: '',
+      password: '',
+      verified: false,
+      restaurants: null
     }
+  }
+
+  updateUserState = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  fetchUserData = async(e) => {
+    e.preventDefault();
+    const initalFetch = await fetch('https://restaurant-res-backend.herokuapp.com/api/v1/restaurants');
+    const restaurants = await initalFetch.json();
+    await this.setState({ restaurants }, () => {
+      this.checkIfMatch();
+    });
+  }
+
+  checkIfMatch = () => {
+    const { restaurants, username } = this.state;
+    const matchingUsername = restaurants.find(restaurant => restaurant.name === username);
+    console.log(matchingUsername);
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.submitRestaurant}>
+        <form onSubmit={this.fetchUserData}>
           <h3>Sign In</h3>
           <input type='text'
                  placeholder='Username'
                  value={this.state.username}
                  name='username'
-                 onChange={(e) => this.updateState(e)}
+                 onChange={(e) => this.updateUserState(e)}
           />
           <input type='text'
                  placeholder='Password'
                  value={this.state.password}
                  name='password'
-                 onChange={(e) => this.updateState(e)}
+                 onChange={(e) => this.updateUserState(e)}
           />
           <button id='sign-up'>Sign In</button>
         </form>
