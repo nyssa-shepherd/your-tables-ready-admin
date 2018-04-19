0d44bba0440fdd86c5ced559e428e1d76a5b0232
Nyssa Keller <nyssa1@gmail.com>
2018-04-18 20:53:29 -0600

    Create AddLocation component

---
+++ src/Locations/AddLocation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AddLocation extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+};
+
+export default AddLocation;
