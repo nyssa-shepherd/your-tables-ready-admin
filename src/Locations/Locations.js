d69313da5ce5ac5cf85fa8c32446a8870c46d54f
Nyssa Keller <nyssa1@gmail.com>
2018-04-18 23:03:14 -0600

    Create fn in the Locations component in order to fetch the locations. Render corresponding locations on the page

--- src/Locations/Locations.js
+++ src/Locations/Locations.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import AddLocation from './AddLocation';
 import './Locations.css';
 
@@ -6,20 +7,48 @@ class Locations extends Component {
   constructor() {
     super();
     this.state = {
-
+      locations: null
     }
   }
 
+  componentDidMount() {
+    this.props.loggedInUser ? this.fetchLocations(this.props.loggedInUser.id) : null;
+  }
+
+  fetchLocations = async(id) => {
+    const initalFetch = await fetch(`https://restaurant-res-backend.herokuapp.com/api/v1/restaurants/${id}/restaurant_details`);
+    const locations = await initalFetch.json();
+    await this.setState({ locations });
+  }
+
   render() {
+    const { locations } = this.state;
+    const renderLocations = locations ? 
+      locations.map(location => {
+        return (
+          <div className='location-area'>
+            <h3>{location.location}</h3>
+            <p>{location.phone_number}</p>
+            <p>{location.tables_open}</p>
+            <p>{location.wait_time}</p>
+          </div>
+        );
+      }) : null;
+      
     return (
       <div>
         <div className='screen'>
           <AddLocation />
+          {renderLocations}
         </div>
       </div>
     );
   }
 };
 
-export default Locations;
+const mapStateToProps = store => ({
+  loggedInUser: store.loggedInUser
+})
+
+export default connect(mapStateToProps)(Locations);
 
